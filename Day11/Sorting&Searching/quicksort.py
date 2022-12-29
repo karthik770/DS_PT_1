ef638b41681472926f68601bbad11ff6fffea99c
Jawaharlal B <veda.vinothini@codeshastra.com>
2022-12-29 22:53:24 +0530

    29/12/2022

--- Day11/Sorting&Searching/quicksort.py
+++ Day11/Sorting&Searching/quicksort.py
@@ -1,26 +1,24 @@
-def partition(array, low, high):
-  pivot = array[high]
-  i = low - 1
-  for j in range(low, high):
-    if array[j] <= pivot:
+def partition(array, low, high): #([8,7,6,1,0,9,2], 0 , 6)
+  pivot = array[high]  # array[6]  ->pivot= 2
+  i = low - 1  # i= -1
+  for j in range(low, high): # --> we are going to set the value less than pivot at left & greater on right 
+    if array[j] <= pivot: # array[0] 8 <= 2 --> False 
       i = i + 1
       (array[i], array[j]) = (array[j], array[i])
-  (array[i + 1], array[high]) = (array[high], array[i + 1])
+  (array[i + 1], array[high]) = (array[high], array[i + 1]) #2, 8
   return i + 1
-def quickSort(array, low, high):
-  if low < high:
-    pi = partition(array, low, high)
+
+def quickSort(array, low, high): #([8,7,6,1,0,9,2], 0 , 6)
+  if low < high: # 0< 6 --> True
+    pi = partition(array, low, high) #([8,7,6,1,0,9,2], 0 , 6)
     quickSort(array, low, pi - 1)
     quickSort(array, pi + 1, high)
 
 
-data = [8, 7, 2, 1, 0, 9, 6]
+data = [8,7,6,1,0,9,2]
 print("Unsorted Array")
 print(data)
-
 size = len(data)
-
-quickSort(data, 0, size - 1)
-
+quickSort(data, 0, size - 1) #  ([8,7,6,1,0,9,2], 0 , 6)
 print('Sorted Array in Ascending Order:')
 print(data)
