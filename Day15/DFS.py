e71893a856ad1873ebaee9d72996aab583fa3392
Jawaharlal B <veda.vinothini@codeshastra.com>
2023-01-09 20:41:34 +0530

    09/01/2023

---
+++ Day15/DFS.py
@@ -0,0 +1,17 @@
+def dfs(graph, start, visited=None):# 0
+    if visited is None:
+        visited = set()
+    visited.add(start) # visited={0}
+    print(start)
+
+    for next in graph[start] - visited:
+        dfs(graph, next, visited)
+    return visited
+
+graph = {'0': set(['1', '2']),
+         '1': set(['0', '3', '4']),
+         '2': set(['0']),
+         '3': set(['1']),
+         '4': set(['2', '3'])}
+
+dfs(graph, '0')
