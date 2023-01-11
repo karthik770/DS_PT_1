93eb0f678dc9e1492daf849c9199315a8ea382ba
Jawaharlal B <veda.vinothini@codeshastra.com>
2023-01-11 22:13:16 +0530

    10/01/2023

---
+++ Spec-DS/Day1/DataWrangling.py
@@ -0,0 +1,10 @@
+import pandas 
+
+data ={
+    'Name': ['S1','S2','S3','S4','S5','S6'],
+    'Marks':[75,80,90,45,99,100],
+    'Gender':['M','F','F','M','F','M']
+}
+
+a= pandas.DataFrame(data)
+print(a)
